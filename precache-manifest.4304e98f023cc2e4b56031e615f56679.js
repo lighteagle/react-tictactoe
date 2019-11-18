cd3991fe410e5bf0cd11859c8a10db4fd87577b6
lighteagle <sofyan.agus18@gmail.com>
2019-11-18 10:30:11 +0700

    Updates

---
+++ precache-manifest.4304e98f023cc2e4b56031e615f56679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fc8a6f815a0d2c579aed4023bc1bf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d40cab937a11c529c79",
+    "url": "/static/css/main.22f901ed.chunk.css"
+  },
+  {
+    "revision": "5eff4f4687488ecc77b7",
+    "url": "/static/js/2.435a99c5.chunk.js"
+  },
+  {
+    "revision": "9d40cab937a11c529c79",
+    "url": "/static/js/main.0ac723c4.chunk.js"
+  },
+  {
+    "revision": "52392f678a86edc4e688",
+    "url": "/static/js/runtime-main.ed59ffbd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
