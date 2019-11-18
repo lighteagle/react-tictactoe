9f6e1d9578a06dd26b4ce75bbb0497eaf48b7d4a
lighteagle <sofyan.agus18@gmail.com>
2019-11-18 10:18:44 +0700

    update

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import Board from "./Board";
-
+import calculateWinner from "./calculateWinner";
 class Game extends Component {
   constructor(props) {
     super(props);
@@ -78,25 +78,5 @@ class Game extends Component {
 }
 
 // ========================================
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
 export default Game;
