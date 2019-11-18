377ef7f4a719b0c5231267b48a366770e130548f
lighteagle <sofyan.agus18@gmail.com>
2019-11-18 12:27:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+import styled from "styled-components";
 import Game from "./components/Game";
+import NavBar from "./components/NavBar";
+
+const Header = styled.h1`
+  text-align: center;
+  background: #000;
+  color: #fff;
+`;
+
+const Wrapper = styled.section`
+  text-align: center;
+  padding: 1em;
+  background: ${props => (props.dark ? "#333" : "#aaa")};
+  min-height: 80vh;
+`;
+
+const Section = styled(Wrapper)`
+  color: white;
+  min-height: 10vh;
+`;
 
 function App() {
   return (
     <div className="App">
-      <Game />
+      <NavBar />
+      <Wrapper light>
+        <Header> My TicTacToe</Header>
+        <Game />
+      </Wrapper>
+      <Section dark> Simple, Easy and Fun</Section>
     </div>
   );
 }
